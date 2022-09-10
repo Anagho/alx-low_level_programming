b0a6f718fd59fec5ed0b4ab20bee468e51ec73a6
Anagho Okiroro Obruthe <anaghosamroyce@gmail.com>
2022-09-09 20:18:31 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - program
+ * Description: Print all possible combinations of single-digit numbers
+ * each followed by a comma and a space, except the last number
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if(n < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		n++;
+	}
+	putchar(10);
+
+	return 0;
+}
