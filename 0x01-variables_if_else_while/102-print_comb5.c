d43516e23cc46937b223610b482a2bbd98d402f1
ANAGHO OKIRORO OBRUTHE <66509601+Anagho@users.noreply.github.com>
2022-09-10 05:45:04 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 /**
- * main - program.
- * Description: Print all possible combinations of two-digit numbers.
- * The two numbers should be separated by a space
- * All numbers should be printed with two digits, 00, 01.
- * You can only use `putchar` and maximum of 8 times
- * Return: (0) on success
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
  */
 int main(void)
 {
-	int i, j, a, b, c, d;
+	int i, j;
+	int a, b, c, d;
 
 	for (i = 0; i < 100; i++)
 	{
-		a = i / 10; /* 1st double number */
-		b = i % 10; /* 1st single number */
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
 
 		for (j = 0; j < 100; j++)
 		{
-			c = j / 10; /* 2nd double number */
-			d = j % 10; /* 2nd single number */
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
 
 			if (a < c || (a == c && b < d))
 			{
@@ -31,13 +38,13 @@ int main(void)
 
 				if (!(a == 9 && b == 8))
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
 	}
 	putchar(10);
-	
+
 	return (0);
 }
