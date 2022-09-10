c7546b575672355465736f3588ba3e92398bc20a
ANAGHO OKIRORO OBRUTHE <66509601+Anagho@users.noreply.github.com>
2022-09-10 05:37:49 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,17 +16,16 @@ int main(void)
 		a = i / 10; /* 1st double number */
 		b = i % 10; /* 1st single number */
 
-		/* 2nd loop */
 		for (j = 0; j < 100; j++)
 		{
 			c = j / 10; /* 2nd double number */
 			d = j % 10; /* 2nd single number */
-			
+
 			if (a < c || (a == c && b < d))
 			{
 				putchar(a + '0');
 				putchar(b + '0');
-				putchar(' ');
+				putchar(32);
 				putchar(c + '0');
 				putchar(d + '0');
 
