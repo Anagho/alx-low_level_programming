791b1fe9df548379178cc98513aa946b6aec0f9f
Anagho Okiroro Obruthe <anaghosamroyce@gmail.com>
2022-09-13 10:07:55 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints to the console _putchar followed by a newline
+ * Return: - ALways 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	-putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
