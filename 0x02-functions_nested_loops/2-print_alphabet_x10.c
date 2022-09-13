8c02bb0bafc1faa655b322863f3880eda1f7f07b
ANAGHO OKIRORO OBRUTHE <66509601+Anagho@users.noreply.github.com>
2022-09-13 19:31:28 +0100

    Task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - function prints the alphabet in lowercase 10 times
+ *print_alphabet_x10 - function prints the alphabet in lowercase 10 times
  */
 
 void print_alphabet_x10(void)
 {
 	char ch;
+	
 	int n = 0;
 
 	while (n < 10)
