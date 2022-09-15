72da506010aeeea72e6987cfa96594eedda61823
Anagho Okiroro Obruthe <anaghosamroyce@gmail.com>
2022-09-15 07:59:28 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints from 0 to 9 digits except 2 and 4
+ * print_most_numbers - Prints from 0 to 9 digits exept 2 and 4
  *
- * Return: None
+ * Return: none
  */
 void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-	{
 		if (i != 2 && i != 4)
-		       _putchar('0' + i);
-	}
+			_putchar('0' + i);
 	_putchar('\n');
-}	
+}
