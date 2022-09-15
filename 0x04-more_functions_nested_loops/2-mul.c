8f356b22f2d061c1bf8427a399a2635b896be9ad
Anagho Okiroro Obruthe <anaghosamroyce@gmail.com>
2022-09-15 07:24:59 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two values
+ * @a: First value
+ * @b: Second value
+ * Return: results of a*b
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
