421918abbfae8229a5a0bbeb20530aeeacc02fb6
Anagho Okiroro Obruthe <anaghosamroyce@gmail.com>
2022-09-19 01:38:00 -0700

    Task 4 - Function to print the reverse of a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i, len = 0;
+	char temp;
+
+	for (; *(s + len) != '\0' ;)
+	{
+		len++;
+	}
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+
+	_putchar('\n');
+}
